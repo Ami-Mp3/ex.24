ebf7431f06f59eb4f4f93fffe867c647bd9d7a5e
Ami-Mp3 <123979206+Ami-Mp3@users.noreply.github.com>
2023-03-01 10:59:28 +0300

    Add files via upload

---
+++ ex.24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program { 
+    
+
+    static void Main(string[] args)
+    {
+
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        if (a == b) 
+        {
+            Console.WriteLine("Равны");
+        }
+        else if (a.Length > b.Length)
+        {
+            Console.WriteLine(a + " больше " + b);
+        }
+        else
+        {
+            Console.WriteLine("Слова не равны. Идёт проверка:");
+            for(int i = 0; i<a.Length; i++)
+            {
+                if (a[i] == b[i])
+                {
+                    Console.WriteLine("+");
+                }
+                
+                else
+                {
+                    Console.WriteLine("Ошибка! " + a[i] + " не равен " + b[i] + ". Позиция № "+ i);
+                }
+            }
+        }
+    }
+}
